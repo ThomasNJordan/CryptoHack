5accd74214de16882caf6ee8100280f3ba8efb58
stand4rd <thomas.noble.jordan@gmail.com>
2023-11-03 11:08:51 -0700

    added exercise 4

--- RSA/Starter/ex4.py
+++ RSA/Starter/ex4.py
@@ -1,6 +1,11 @@
+from src.gcd import extended_gcd
+
 p = 857504083339712752489993810777  # primes
 q = 1029224947942998075080348647219 # primes
 e = 65537 # exponent
 
 N = (p-1)*(q-1) # Totient
 
+# Decryption key is Bézout coefficients' old_s
+d = extended_gcd(e, N)
+print("Decryption Key is: ", d[0])
